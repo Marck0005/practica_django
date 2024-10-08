b6913d32dc590d389051a3f2182c82bb29611d87
Marcos Mela <melamarcos94@gmail.com>
2024-10-08 20:06:11 +0200

    [FIX] added path

--- relecloud/urls.py
+++ relecloud/urls.py
@@ -5,10 +5,10 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('destinations', views.destinations, name='destinations'),
+    path('cruises', views.cruises, name='cruises'),
     path('destination/<int:pk>', views.DestinationDetailView.as_view(), name= 'destination_detail'),
     path('destination/create', views.DestinationCreateView.as_view(), name= 'destination_form'),
     path('destination/<int:pk>/update', views.DestinationUpdateView.as_view(), name= 'destination_form'),
     path('destination/<int:pk>/delete', views.DestinationDeleteView.as_view(), name= 'destination_confirm_delete'),
     
-
 ]
